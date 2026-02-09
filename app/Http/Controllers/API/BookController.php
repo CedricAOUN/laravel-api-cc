4ff3d7024b850a37adb9062bb323e0b3be8d7f41
CedricA <cedric.j.aoun@gmail.com>
2026-02-09 14:44:58 +0100

    exo 2: add BookResource with BookController and routes

---
+++ app/Http/Controllers/API/BookController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BookResource;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return BookResource::collection(Book::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|min:3|max:255',
+            'author' => 'required|string|min:3|max:100',
+            'summary' => 'required|string|min:10|max:500',
+            'isbn' => 'required|string|size:13|unique:books,isbn',
+        ]);
+        return new BookResource(Book::create($validated));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return new BookResource(Book::find($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $book = Book::find($id);
+        $validated = $request->validate([
+            'title' => 'required|string|min:3|max:255',
+            'author' => 'required|string|min:3|max:100',
+            'summary' => 'required|string|min:10|max:500',
+            'isbn' => 'required|string|size:13|unique:books,isbn,',
+        ]);
+        $book->update($validated);
+        return new BookResource($book);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $book = Book::find($id);
+        $book->delete();
+        return response()->json(null, 204);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\API\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books', [BookController::class, 'index']);
+Route::post('/books/create', [BookController::class, 'store']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::put('/books/{id}/edit', [BookController::class, 'update']);
+Route::delete('/books/{id}/delete', [BookController::class, 'destroy']);
