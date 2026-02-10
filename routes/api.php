980a2d3a7415d732795163f1dbc570f5f99733eb
CedricA <cedric.j.aoun@gmail.com>
2026-02-10 11:03:02 +0100

    exo 3.2: make only GET routes public

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,9 @@ Route::get('/ping', function () {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/books', [BookController::class, 'index']);
     Route::post('/books', [BookController::class, 'store']);
-    Route::get('/books/{id}', [BookController::class, 'show']);
     Route::put('/books/{id}', [BookController::class, 'update']);
+    Route::patch('/books/{id}', [BookController::class, 'update']);
     Route::delete('/books/{id}', [BookController::class, 'destroy']);
 });
 
