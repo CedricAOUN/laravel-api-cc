f53d623a26c7e92cfd1a4296568745ebc2848075
CedricA <cedric.j.aoun@gmail.com>
2026-02-13 10:17:11 +0100

    exo 6: add basic test for User model

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_uses_professional_email(): void
+    {
+        $user = User::factory()->create(['email' => 'john@company.com']);
+        $this->assertTrue($user->usesProfessionalEmail());
+    }
+    public function test_user_uses_personal_email(): void
+    {
+        $user = User::factory()->create(['email' => 'john@gmail.com']);
+        $this->assertFalse($user->usesProfessionalEmail());
+    }
+}
